80f3e93e5cf150cea1aefb56b8fccd18c43defe6
rogerwelin <roger.welin@icloud.com>
2021-09-15 20:32:20 +0200

    adding test

---
+++ internal/commands/version_test.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestVersion(t *testing.T) {
+
+	expectedStr := "Cfnctl version v0.1.0\n\nYour version of Cfnctl is out of date. The latest version is v0.14.0\n"
+	buf := &bytes.Buffer{}
+	err := OutputVersion("0.1.0", buf)
+	if err != nil {
+		t.Errorf("Expected ok but got: %v", err)
+	}
+
+	if buf.String() != expectedStr {
+		t.Errorf("Expected str:\n %s but got:\n %s", expectedStr, buf.String())
+	}
+
+	expectedStr2 := "Cfnctl version v1.0.0\n"
+	buf.Reset()
+
+	err = OutputVersion("1.0.0", buf)
+	if err != nil {
+		t.Errorf("Expected ok but got: %v", err)
+	}
+
+	if buf.String() != expectedStr2 {
+		t.Errorf("Expected str:\n%s but got:\n %s", expectedStr2, buf.String())
+	}
+
+}
